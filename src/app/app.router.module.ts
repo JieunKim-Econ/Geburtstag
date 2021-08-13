030772093f3fedd63de4d1211cf770f2f515ddf2
Jieun <71706597+JieunKim-Econ@users.noreply.github.com>
2021-08-14 01:10:58 +0200

    Update and rename app-routing.module.ts to app.router.module.ts

--- src/app/app.router.module.ts
+++ src/app/app.router.module.ts
@@ -1,4 +1,3 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { GiftComponent } from './pages/gift/gift.component';
@@ -7,7 +6,7 @@ import { CameraComponent } from './pages/camera/camera.component';
 import { MainImgComponent } from './pages/main-img/main-img.component';
 import { HbdComponent } from './pages/hbd/hbd.component';
 
-const routes: Routes = [
+const AppRoutes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' }, // 첫 화면
   { path: 'main', component: MainImgComponent, }, 
   { path: 'gift', component: GiftComponent, }, 
@@ -16,8 +15,4 @@ const routes: Routes = [
   { path: 'hbd', component: HbdComponent}
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRouterModule = RouterModule.forRoot(AppRoutes, {useHash: true});
